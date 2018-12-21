78118fdc898cfe999e81bfd436f128ed4f8b8c3a
carolinux <carolinegr@gmail.com>
2018-12-21 11:59:38 +0000

    queue to retry if exception

--- datauploader/tasks.py
+++ datauploader/tasks.py
@@ -1,8 +1,5 @@
 """
 Asynchronous tasks that update data in Open Humans.
-These tasks:
-  1. delete any current files in OH if they match the planned upload filename
-  2. adds a data file
 """
 import logging
 
@@ -37,26 +34,31 @@ def fetch_googlefit_data(oh_id):
     '''
     Fetches all of the googlefit data for a given user
     '''
-    current_dt = datetime.utcnow()
-    oh_member = OpenHumansMember.objects.get(oh_id=oh_id)
-    gf_member = oh_member.googlefit_member
-    oh_access_token = oh_member.get_access_token()
-    gf_access_token = gf_member.get_access_token()
+    try:
+        current_dt = datetime.utcnow()
+        oh_member = OpenHumansMember.objects.get(oh_id=oh_id)
+        gf_member = oh_member.googlefit_member
+        oh_access_token = oh_member.get_access_token()
+        gf_access_token = gf_member.get_access_token()
 
-    basenames_to_ids = get_existing_basenames_to_ids(oh_member)
+        basenames_to_ids = get_existing_basenames_to_ids(oh_member)
 
-    filesmonth = get_googlefit_data(oh_access_token, gf_access_token, current_dt)
-    for fn, month in filesmonth:
-        api.upload_aws(fn, create_metadata(month),
-                              oh_access_token,
-                              project_member_id=oh_id)
-        basename = os.path.basename(fn)
-        if basename in basenames_to_ids:
-            file_id_to_delete = basenames_to_ids[basename]
-            api.delete_file(oh_access_token, file_id=file_id_to_delete)
+        filesmonth = get_googlefit_data(oh_access_token, gf_access_token, current_dt)
+        for fn, month in filesmonth:
+            api.upload_aws(fn, create_metadata(month),
+                                  oh_access_token,
+                                  project_member_id=oh_id)
+            basename = os.path.basename(fn)
+            if basename in basenames_to_ids:
+                file_id_to_delete = basenames_to_ids[basename]
+                api.delete_file(oh_access_token, file_id=file_id_to_delete)
 
-    gf_member.last_updated = arrow.now().format()
-    gf_member.save()
+        gf_member.last_updated = arrow.now().format()
+        gf_member.save()
+
+    except:
+        # queue to retry later
+        fetch_googlefit_data.apply_async(args=[oh_id], countdown=3600)
 
 
 def get_existing_basenames_to_ids(oh_member):
