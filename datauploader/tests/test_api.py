704ef19adbe94442070f349d09c7e2062cbd6097
carolinux <carolinegr@gmail.com>
2018-12-21 11:44:32 +0000

    Add tests for generate_monthly_range

--- datauploader/tests/test_api.py
+++ datauploader/tests/test_api.py
@@ -1,4 +1,47 @@
+from datetime import datetime
 from datauploader import googlefit_api as API
+
+
+def monthly_ranges_should_work_for_long_range_spanning_many_months():
+    start_dt = datetime(2017, 9, 2, 4, 4, 2)
+    end_dt = datetime(2018, 1, 5, 4, 4, 6)
+    ranges = list(API.generate_monthly_ranges(start_dt, end_dt))
+    expected = [(start_dt, API.end_of_day(datetime(2017, 9, 30))),
+                (API.start_of_day(datetime(2017, 10, 1)), API.end_of_day(datetime(2017, 10, 31))),
+                (API.start_of_day(datetime(2017, 11, 1)), API.end_of_day(datetime(2017, 11, 30))),
+                (API.start_of_day(datetime(2017, 12, 1)), API.end_of_day(datetime(2017, 12, 31))),
+                (API.start_of_day(datetime(2018, 1, 1)), end_dt)
+                ]
+
+    assert (ranges == expected)
+
+
+def monthly_ranges_should_work_for_short_range_within_same_month():
+    start_dt = datetime(2017, 9, 2, 4, 4, 2)
+    end_dt = datetime(2017, 9, 5, 4, 4, 6)
+    ranges = list(API.generate_monthly_ranges(start_dt, end_dt))
+    expected = [(start_dt, end_dt)]
+    assert (ranges == expected)
+
+
+def monthly_ranges_should_work_for_same_date():
+    start_dt = datetime(2017, 9, 2, 4, 4, 2)
+    end_dt = datetime(2017, 9, 2, 23, 4, 2)
+    ranges = list(API.generate_monthly_ranges(start_dt, end_dt))
+    expected = [(start_dt, end_dt)]
+    assert (ranges == expected)
+
+
+def monthly_ranges_should_work_for_leap_years():
+    start_dt = datetime(2016, 1, 2, 4, 4, 2)
+    end_dt = datetime(2016, 3, 5, 4, 4, 6)
+    ranges = list(API.generate_monthly_ranges(start_dt, end_dt))
+    expected = [(start_dt, API.end_of_day(datetime(2016, 1, 31))),
+                (API.start_of_day(datetime(2016, 2, 1)), API.end_of_day(datetime(2016, 2, 29))),
+                (API.start_of_day(datetime(2016, 3, 1)), end_dt)
+                ]
+
+    assert (ranges == expected)
 
 
 def merging_datasets_without_daily_overlap_should_work():
@@ -28,14 +71,13 @@ def merging_datasets_without_daily_overlap_should_work():
     assert (gf2.merge(gf1) == expected)
 
 
-
 def merging_datasets_with_daily_overlap_should_work():
     pairs = [('steps', 'from_gps')]
     data_types = ['steps']
 
     dataset1 = {'steps': {'from_gps': {'2017-10-01': ['data1'], '2017-10-09': ['data2']}}}
 
-    dataset2 = {'steps': {'from_gps': {'2017-10-09': ['data2','data3'],
+    dataset2 = {'steps': {'from_gps': {'2017-10-09': ['data2', 'data3'],
                                        '2017-10-11': ['data4']}}}
 
     # for the day present in both datasets -> we should get the longest dataset, ie '2017-10-09': ['data2', 'data3']
