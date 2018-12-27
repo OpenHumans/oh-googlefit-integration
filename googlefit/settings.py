6f29475263eaa20fda0db7d56022d84adc0b87f3
carolinux <carolinegr@gmail.com>
2018-12-27 14:07:45 +0000

    Disable debug by default

--- googlefit/settings.py
+++ googlefit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.getenv('DEBUG', '').lower() == 'false' else True
+DEBUG = True if os.getenv('DEBUG', '').lower() == 'true' else False
 
 REMOTE = True if os.getenv('REMOTE', '').lower() == 'true' else False
 
