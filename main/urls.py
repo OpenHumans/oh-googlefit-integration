72f09333f6f3a026f49d7ca74edad81918dd90cf
Mad Price Ball <mpball@gmail.com>
2019-02-11 12:11:17 -0800

    Update to standard templates and flow

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('setup/', views.setup, name='setup'),
+    path('logout/', views.logout_user, name='logout'),
     path('complete/', views.complete_googlefit, name='complete_googlefit'),
     path('authorize/', views.authorize_googlefit, name='authorize_googlefit'),
     path('dashboard/', views.dashboard, name='dashboard'),
