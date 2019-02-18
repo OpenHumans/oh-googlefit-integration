3aeb0b02be06f6a2fb90fb2e53089927f9cd7678
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2019-02-18 14:50:43 -0800

    remove custom storage

--- googlefit/storage.py
+++
@@ -1,4 +0,0 @@
-from whitenoise.storage import CompressedManifestStaticFilesStorage
-
-class NonStrictCompressedManifestStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    manifest_strict = False
